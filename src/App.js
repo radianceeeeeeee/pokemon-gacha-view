040ff455c94b92fd20cab9e42dc8dae9f4bb585f
radianceeeeeeee <97012089+radianceeeeeeee@users.noreply.github.com>
2024-10-26 19:16:44 +0800

    Added template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [isListView, setIsListView] = useState(true);
+
+  function handleChange(e) {
+    setIsListView(!isListView);
+  }
+
+  const fetchRandomPokemons = async () => {
+    console.log(isListView);
+    const randomPokemonPromises = [];
+
+    for (let i = 0; i < 6; i++) {
+      const randomNum = Math.floor(Math.random() * 1118) + 1;
+      const url = `https://pokeapi.co/api/v2/pokemon/${randomNum}`;
+      randomPokemonPromises.push(axios.get(url));
+    }
+
+    try {
+      const responses = await Promise.all(randomPokemonPromises);
+      setPokemons(responses.map(response => response.data));
+    } catch (error) {
+      console.error('Error fetching Pokémon:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header className='App-Header'>
+        <div className="App-Title">Pokemon Gacha</div>
+        <label className="App-ToggleView">
+          <input type="checkbox"></input>
+          <span>List View</span>
+        </label>
+        <button className="App-FetchButton" onClick={fetchRandomPokemons}>Pull!</button>
+      </header> 
+
+      <div className='App-Body'>
+
+      </div>
+
+      <footer className='App-Footer'>
+        Used the API <a href="https://pokeapi.co/docs/v2">PokeAPI</a>
+      </footer>
     </div>
   );
 }
